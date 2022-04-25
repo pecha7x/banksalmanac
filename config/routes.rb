ea3abdd4bc661898c3031db632748988eb6f9dae
pecha7x <pecha7x@gmail.com>
2022-04-25 23:50:17 +0400

    Implement React Ui-Material Admin Template

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # root 'pages#index'
+  root 'pages#index'
 
   namespace :api do
     resources :banks, only: %i[index show update]
   end
 
-  # get '*path', to: 'pages#index', via: :all
+  get '*path', to: 'pages#index', via: :all
 end
